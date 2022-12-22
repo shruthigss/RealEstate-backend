a79922f139f7d84226e888954e0702a0a8db9d3a
Sandeep <sandeep.kastu@gmail.com>
2022-12-22 18:31:34 +0530

    second commit

--- src/routes/properties.js
+++ src/routes/properties.js
@@ -1,62 +1,63 @@
-const router =require('express').Router();
-const Pschema=require('../models/propertySchema')
+const router = require("express").Router();
+const propertySchema = require("../models/propertySchema");
 
-
-router.get('/allproperties',(req,res)=>{
-    Pschema.find()
-    .populate('postedBy',"name")
-    .then(properties=>{
-        res.json({properties})
-    }).catch(err=>{
-        console.log(err)
+router.get("/allproperties", (req, res) => {
+  Pschema.find()
+    .populate("postedBy", "name")
+    .then((properties) => {
+      res.json({ properties });
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.post('/addproperty',(req,res)=>{
-    
-    const property=new Pschema({
-        Property_type,
-         Negotable,
-        Price,
-        OwnerShip,
-        Property_age:req.body.Property_age,
-        Property_Approved,
-        Property_Description,
-        Bank_loan,
-        Length,
-        Breadth,
-        Total_area,
-        area_unit,
-        No_of_BHK,
-        No_of_floor,
-        Attached,
-        Western_toilet,
-        Furnished,
-        Car_parking,
-        Lift,
-        Electricity,
-        Facing,
-        Name,
-        Mobile,
-        Posted_by,
-        Sale_type,
-        Featured_package,
-        PPD_Package,
-        Email,
-        City,
-        Area,
-        Pincode,
-        Address,
-        Landmark,
-        Latitude,
-        Longitude
- 
+router.post("/addproperty", (req, res) => {
+  const property = new propertySchema({
+    Property_type,
+    Negotable,
+    Price,
+    OwnerShip,
+    Property_age: req.body.Property_age,
+    Property_Approved,
+    Property_Description,
+    Bank_loan,
+    Length,
+    Breadth,
+    Total_area,
+    area_unit,
+    No_of_BHK,
+    No_of_floor,
+    Attached,
+    Western_toilet,
+    Furnished,
+    Car_parking,
+    Lift,
+    Electricity,
+    Facing,
+    Name,
+    Mobile,
+    Posted_by,
+    Sale_type,
+    Featured_package,
+    PPD_Package,
+    Email,
+    City,
+    Area,
+    Pincode,
+    Address,
+    Landmark,
+    Latitude,
+    Longitude,
+  });
+  property
+    .save()
+    .then((result) => {
+      res.json({ property: result });
     })
-    property.save().then(result=>{
-        res.json({property:result})
-    }).catch(err=>{
-        console.log(err)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-module.exports=router
+module.exports = router;
